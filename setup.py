544ac1e0cb31253d815598fad4838c3f32a3b001
Daniel M. Martin <danielmmartin@gmail.com>
2021-07-26 07:38:29 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='graph-dfs',
+    version='0.0.1',
+    description='Module for creating graphs and performing depth first searches.',
+    py_modules=['dfs', 'dfs/graph.py', 'dfs/dfs.py', 'dfs/test_dfs.py'],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+        "License :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    long_description=long_description
+)
